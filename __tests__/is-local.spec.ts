c83cba9c5c763c08bfd9b4628d6ae67de300b92a
Faruq Alli-Balogun <ab.faruq@sten.agency>
2024-02-11 01:37:42 +0100

    fix(base): local state can not be gotten from process.env.IS_LOCAL

---
+++ __tests__/is-local.spec.ts
@@ -0,0 +1,14 @@
+import { isLocal } from '../is-local.js';
+
+describe.each`
+  stage              | expectedValue
+  ${''}              | ${true}
+  ${undefined}       | ${true}
+  ${'development'}   | ${false}
+  ${'production'}    | ${false}
+  ${'SSTLOCAL-User'} | ${true}
+`('when stage is `$stage`', ({ stage, expectedValue }) => {
+  it(`should return ${expectedValue}`, () => {
+    expect(isLocal(stage)).toEqual(expectedValue);
+  });
+});
